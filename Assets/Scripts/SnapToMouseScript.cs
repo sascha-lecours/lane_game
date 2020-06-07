7fd5d4db0ef1149d31066f3d9205ec4d0e7c84db
slecours <sascha.lecours@gmail.com>
2020-06-07 14:02:36 -0400

    Changed drag-drop script to snapToMouse script. Heading and targeting now bound to mouse 0 and 1

---
+++ Assets/Scripts/SnapToMouseScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnapToMouseScript : MonoBehaviour
+{
+    public string snapHotkey = null;
+    private Plane dragPlane;
+
+    private Camera myMainCamera;
+
+    void Start()
+    {
+        myMainCamera = Camera.main; // Camera.main is expensive ; cache it here
+    }
+
+    void snapToMousePosition()
+    {
+        dragPlane = new Plane(myMainCamera.transform.forward, transform.position);
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition);
+       
+        float planeDist;
+        dragPlane.Raycast(camRay, out planeDist);
+        transform.position = camRay.GetPoint(planeDist);
+    }
+
+    void Update()
+    {
+        if (snapHotkey != null)
+        {
+            if (Input.GetKey(snapHotkey))
+            {
+                snapToMousePosition();
+            }
+        }
+    }
+}
