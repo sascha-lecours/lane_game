34a76272179a2d576fcdf01560932351b9d41ef5
sascha-lecours <sascha.lecours@gmail.com>
2020-06-15 22:05:26 -0400

    Added more sophisticated wave mgmt to enemy spawner, some visual tweaks to subs and torpedoes

--- Assets/Scripts/EnemySpawnerScript.cs
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 
 public class EnemySpawnerScript : MonoBehaviour
 {
-    public Transform[] possibleSpawns;
+    public Transform[] easySpawns;
+    public Transform[] mediumSpawns;
+    public Transform[] hardSpawns;
+    public int numberOfEasyWaves = 4;
+    public int numberOfMediumWaves = 6;
+    public int numberOfHardWaves = 6;
     public float initialSpawnInterval = 6f; // Time in seconds between waves at start
 
+    private int waveCounter = 0;
     private float waveTimer = 0f;
     private float spawnInterval;
+    private int mediumWavesStart;
+    private int hardWavesStart;
     
 
     // Start is called before the first frame update
     void Start()
     {
         spawnInterval = initialSpawnInterval;
+        mediumWavesStart = numberOfEasyWaves;
+        hardWavesStart = numberOfEasyWaves + numberOfMediumWaves;
     }
 
     void spawnSomething()
     {
-        // ##TODO: right now this just spawns completely at random. Later this will be more sophisticated
-        var index = UnityEngine.Random.Range(0, possibleSpawns.Length);
-        var newSpawn = Instantiate(possibleSpawns[index]) as Transform;
+        var currentDifficulty = easySpawns;
+        if (waveCounter >= mediumWavesStart) { currentDifficulty = mediumSpawns; }
+        if (waveCounter >= hardWavesStart) { currentDifficulty = hardSpawns; }
+        var index = UnityEngine.Random.Range(0, currentDifficulty.Length);
+        var newSpawn = Instantiate(currentDifficulty[index]) as Transform;
         newSpawn.position = transform.position;
     }
 
@@ -32,6 +44,7 @@ public class EnemySpawnerScript : MonoBehaviour
         waveTimer += Time.deltaTime;
         if (waveTimer >= spawnInterval)
         {
+            waveCounter++;
             spawnSomething();
             waveTimer = 0f;
         }
