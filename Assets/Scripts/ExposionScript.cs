560ec92a7c9797e84f86cf427fb3ace1f09af5b8
sascha-lecours <sascha.lecours@gmail.com>
2020-06-18 21:55:26 -0400

    Added explosion effects and damaged hulls for many units.

---
+++ Assets/Scripts/ExposionScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExposionScript : MonoBehaviour
+{
+    public float shakeMagnitude = 0.5f;
+
+    void Start()
+    {
+        // Limited time to live
+        Destroy(gameObject, 3); // 3sec
+
+        // Explosion Sound
+        SoundEffectsHelper.Instance.MakeExplosionSound();
+
+        // Add screenshake
+        Camera.main.GetComponent<CameraShakeScript>().addShake(shakeMagnitude); 
+    }
+}
