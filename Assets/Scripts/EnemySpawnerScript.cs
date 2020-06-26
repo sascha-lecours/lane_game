777c98a40b03a80d11d64a79e3a4541172b88d7a
sascha-lecours <sascha.lecours@gmail.com>
2020-06-25 22:23:16 -0400

    added ramping difficulty over time

--- Assets/Scripts/EnemySpawnerScript.cs
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -12,12 +12,16 @@ public class EnemySpawnerScript : MonoBehaviour
     public int numberOfMediumWaves = 6;
     public int numberOfHardWaves = 6;
     public float initialSpawnInterval = 6f; // Time in seconds between waves at start
+    public float shrinkTimeInterval = 0.15f; // Amount of time wave timer shortens once it starts to
+    public int wavesPerIntervalShrink = 6; // Once hard waves start, the interval will shrink each time this many waves pass
 
     private int waveCounter = 0;
     private float waveTimer = 0f;
     private float spawnInterval;
     private int mediumWavesStart;
     private int hardWavesStart;
+    private int intervalCounter = 0;
+    private float minimumInterval = 3.5f; // Won't shrink any smaller than this.
     
 
     // Start is called before the first frame update
@@ -32,10 +36,20 @@ public class EnemySpawnerScript : MonoBehaviour
     {
         var currentDifficulty = easySpawns;
         if (waveCounter >= mediumWavesStart) { currentDifficulty = mediumSpawns; }
-        if (waveCounter >= hardWavesStart) { currentDifficulty = hardSpawns; }
+        if (waveCounter >= hardWavesStart) {
+            currentDifficulty = hardSpawns;
+            intervalCounter += 1;
+        }
         var index = UnityEngine.Random.Range(0, currentDifficulty.Length);
         var newSpawn = Instantiate(currentDifficulty[index]) as Transform;
         newSpawn.position = transform.position;
+
+        if (intervalCounter >= wavesPerIntervalShrink)
+        {
+            // Shrink the timer interval each time until it reaches the minimum
+            spawnInterval = Math.Max(minimumInterval, spawnInterval - shrinkTimeInterval);
+            intervalCounter = 0;
+        }
     }
 
     // Update is called once per frame
